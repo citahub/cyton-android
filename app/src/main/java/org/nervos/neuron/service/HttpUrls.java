f6823985631d540114faacc19c84bf399822ca4c
duanyytop <duanyytop@gmail.com>
2018-08-15 20:26:45 +0800

    update appchain node

--- app/src/main/java/org/nervos/neuron/service/HttpUrls.java
+++ app/src/main/java/org/nervos/neuron/service/HttpUrls.java
@@ -25,11 +25,8 @@ public class HttpUrls {
 
 
     // discover page config information
-    public static final String DISCOVER_URL = "http://47.97.171.140:8866/dapps";
-    public static final String INNER_URL = "http://47.97.171.140:8866/";
-
-//    public static final String DISCOVER_URL = "http://192.168.10.24:3000/dapps";
-//    public static final String INNER_URL = "http://192.168.10.24:3000/";
+    public static final String DISCOVER_URL = "https://dapp.cryptape.com/dapps";
+    public static final String INNER_URL = "https://dapp.cryptape.com/dapps";
 
     //Token Currency ID Price
     public static final String TOKEN_ID = "https://api.coinmarketcap.com/v2/listings/";
