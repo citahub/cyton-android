5c2669ed2862f42d554171116ee53642060a35ac
baojun <bj805887485@aliyun.com>
2018-08-28 16:22:30 +0800

    feature simple dialog ui

---
+++ app/src/main/java/org/nervos/neuron/util/ScreenUtils.java
@@ -0,0 +1,31 @@
+package org.nervos.neuron.util;
+
+import android.app.Activity;
+import android.content.Context;
+import android.graphics.Bitmap;
+import android.graphics.Rect;
+import android.util.DisplayMetrics;
+import android.view.View;
+import android.view.WindowManager;
+
+public class ScreenUtils {
+	private ScreenUtils() {
+		throw new UnsupportedOperationException("cannot be instantiated");
+	}
+
+	public static int getScreenWidth(Context context) {
+		WindowManager wm = (WindowManager) context
+				.getSystemService(Context.WINDOW_SERVICE);
+		DisplayMetrics outMetrics = new DisplayMetrics();
+		wm.getDefaultDisplay().getMetrics(outMetrics);
+		return outMetrics.widthPixels;
+	}
+
+	public static int getScreenHeight(Context context) {
+		WindowManager wm = (WindowManager) context
+				.getSystemService(Context.WINDOW_SERVICE);
+		DisplayMetrics outMetrics = new DisplayMetrics();
+		wm.getDefaultDisplay().getMetrics(outMetrics);
+		return outMetrics.heightPixels;
+	}
+}
