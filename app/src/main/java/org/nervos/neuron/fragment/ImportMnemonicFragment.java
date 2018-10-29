2c7cabc42109489379226636364d519bbd2c4872
baojun <bj805887485@aliyun.com>
2018-10-29 16:49:55 +0800

    code standards

--- app/src/main/java/org/nervos/neuron/fragment/ImportMnemonicFragment.java
+++ app/src/main/java/org/nervos/neuron/fragment/ImportMnemonicFragment.java
@@ -183,8 +183,9 @@ public class ImportMnemonicFragment extends BaseFragment {
         passwordEdit.post(() -> {
             Toast.makeText(getContext(), R.string.wallet_export_success, Toast.LENGTH_SHORT).show();
             dismissProgressBar();
-            if (FingerPrintController.getInstance(getActivity()).isSupportFingerprint() && !SharePrefUtil.getBoolean(ConstUtil
-                    .Fingerprint, false) && !SharePrefUtil.getBoolean(ConstUtil.FINGERPRINT_TIP, false)) {
+            if (FingerPrintController.getInstance(getActivity()).isSupportFingerprint() &&
+                    !SharePrefUtil.getBoolean(ConstUtil.Fingerprint, false) &&
+                    !SharePrefUtil.getBoolean(ConstUtil.FINGERPRINT_TIP, false)) {
                 Intent intent = new Intent(getActivity(), ImportFingerTipActivity.class);
                 startActivity(intent);
                 SharePrefUtil.putBoolean(ConstUtil.FINGERPRINT_TIP, true);
