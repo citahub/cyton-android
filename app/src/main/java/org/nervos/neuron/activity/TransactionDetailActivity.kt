390c59715004c73b327c38e96434538e3b5e66ee
baojun <bj805887485@aliyun.com>
2018-11-16 15:35:16 +0800

    bugfix add eth contract created

--- app/src/main/java/org/nervos/neuron/activity/TransactionDetailActivity.kt
+++ app/src/main/java/org/nervos/neuron/activity/TransactionDetailActivity.kt
@@ -52,7 +52,10 @@ class TransactionDetailActivity : NBaseActivity() {
 
         tv_transaction_number.text = transactionItem!!.hash
         tv_transaction_sender.text = transactionItem!!.from
-        tv_transaction_receiver.text = if (ConstantUtil.RPC_RESULT_ZERO == transactionItem!!.to) resources.getString(R.string.contract_create) else transactionItem!!.to
+        tv_transaction_receiver.text =
+                if (ConstantUtil.RPC_RESULT_ZERO == transactionItem!!.to || transactionItem!!.to.isEmpty())
+                    resources.getString(R.string.contract_create)
+                else transactionItem!!.to
         if (!TextUtils.isEmpty(transactionItem!!.gasPrice)) {
             tv_chain_name.text = ConstantUtil.ETH_MAINNET
             val gasPriceBig = BigInteger(transactionItem!!.gasPrice)
@@ -82,8 +85,9 @@ class TransactionDetailActivity : NBaseActivity() {
                     .subscribe(object : NeuronSubscriber<String>() {
                         override fun onNext(price: String) {
                             super.onNext(price)
-                            tv_transaction_gas.text = NumberUtil.getEthFromWeiForStringDecimal8(Numeric.toBigInt(transactionItem!!.gasUsed)
-                                    .multiply(Numeric.toBigInt(price))) + transactionItem!!.nativeSymbol
+                            tv_transaction_gas.text =
+                                    NumberUtil.getEthFromWeiForStringDecimal8(Numeric.toBigInt(transactionItem!!.gasUsed)
+                                            .multiply(Numeric.toBigInt(price))) + transactionItem!!.nativeSymbol
                         }
                     })
         }
@@ -91,7 +95,7 @@ class TransactionDetailActivity : NBaseActivity() {
         tv_transaction_blockchain_time.text = transactionItem!!.date
 
         tv_transaction_receiver!!.setOnClickListener {
-            if (transactionItem!!.to != ConstantUtil.RPC_RESULT_ZERO)
+            if (transactionItem!!.to != ConstantUtil.RPC_RESULT_ZERO && !transactionItem!!.to.isEmpty())
                 copyText(transactionItem!!.to)
         }
         tv_transaction_sender!!.setOnClickListener { copyText(transactionItem!!.from) }
