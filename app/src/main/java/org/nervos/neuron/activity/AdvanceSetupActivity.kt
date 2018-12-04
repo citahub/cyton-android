39a857131b621cb6c377445cf7cbabda6c24c3de
duanyytop <duanyytop@gmail.com>
2018-12-04 11:24:34 +0800

    add advance setup activity and layout

---
+++ app/src/main/java/org/nervos/neuron/activity/AdvanceSetupActivity.kt
@@ -0,0 +1,23 @@
+package org.nervos.neuron.activity
+
+/**
+ * Created by duanyytop on 2018/12/4.
+ */
+class AdvanceSetupActivity : NBaseActivity() {
+
+    override fun getContentLayout(): Int {
+        return 0
+    }
+
+    override fun initView() {
+
+    }
+
+    override fun initData() {
+
+    }
+
+    override fun initAction() {
+
+    }
+}
