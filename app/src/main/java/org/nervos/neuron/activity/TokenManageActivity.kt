e1b7d85833b5b959ad2e2a353abd38a00a5d3937
baojun <baojun19920409@gmail.com>
2018-12-05 15:31:03 +0800

    add token item dragging

--- app/src/main/java/org/nervos/neuron/activity/TokenManageActivity.kt
+++ app/src/main/java/org/nervos/neuron/activity/TokenManageActivity.kt
@@ -1,13 +1,16 @@
 package org.nervos.neuron.activity
 
+import android.annotation.SuppressLint
 import android.support.v7.widget.LinearLayoutManager
 import android.support.v7.widget.RecyclerView
+import android.support.v7.widget.helper.ItemTouchHelper
 import android.text.TextUtils
-import android.view.KeyEvent
 import android.view.LayoutInflater
+import android.view.MotionEvent
 import android.view.View
 import android.view.ViewGroup
 import android.widget.ImageView
+import android.widget.RelativeLayout
 import android.widget.Switch
 import android.widget.TextView
 import kotlinx.android.synthetic.main.activity_token_manage.*
@@ -18,42 +21,54 @@ import org.nervos.neuron.item.TokenItem
 import org.nervos.neuron.util.TokenLogoUtil
 import org.nervos.neuron.util.db.DBWalletUtil
 import org.nervos.neuron.view.TitleBar
-import java.util.*
 
 /**
  * Created by BaojunCZ on 2018/12/3.
  */
 class TokenManageActivity : NBaseActivity() {
 
-    private var titleBar: TitleBar? = null
-    private var tokenList: List<TokenItem> = ArrayList()
-    private val adapter = TokenAdapter()
+    private lateinit var mTitleBar: TitleBar
+    private var mTokenList = mutableListOf<TokenItem>()
+    private val mAdapter = TokenAdapter()
+    //token item status true:normal false:draged
+    private var mTokenStatus = true
+    private val mItemTouchHelper = ItemTouchHelper(SimpleItemTouchCallback())
 
     override fun getContentLayout(): Int {
         return R.layout.activity_token_manage
     }
 
     override fun initView() {
-        titleBar = findViewById(R.id.title)
+        mTitleBar = findViewById(R.id.title)
     }
 
     override fun initData() {
         addCustomToken()
         token_recycler.layoutManager = LinearLayoutManager(mActivity)
-        token_recycler.adapter = adapter
+        token_recycler.adapter = mAdapter
+        mItemTouchHelper.attachToRecyclerView(token_recycler)
     }
 
     override fun initAction() {
-        titleBar!!.setOnRightClickListener { }
-        titleBar!!.setOnLeftClickListener {
+        mTitleBar.setOnRightClickListener { }
+        mTitleBar.setOnLeftClickListener {
             EventBus.getDefault().post(AddTokenRefreshEvent())
             finish()
+        }
+        mTitleBar.setOnRightClickListener {
+            if (mTokenStatus) {
+                mTitleBar.rightText = resources.getString(R.string.finish)
+            } else {
+                mTitleBar.rightText = resources.getString(R.string.edit)
+            }
+            mTokenStatus = !mTokenStatus
+            mAdapter.notifyDataSetChanged()
         }
     }
 
     private fun addCustomToken() {
-        tokenList = DBWalletUtil.getCurrentWallet(mActivity).tokenItems
-        adapter.notifyDataSetChanged()
+        mTokenList = DBWalletUtil.getCurrentWallet(mActivity).tokenItems
+        mAdapter.notifyDataSetChanged()
     }
 
     internal inner class TokenAdapter : RecyclerView.Adapter<RecyclerView.ViewHolder>() {
@@ -61,51 +76,94 @@ class TokenManageActivity : NBaseActivity() {
             return TokenViewHolder(LayoutInflater.from(mActivity).inflate(R.layout.item_token_info, parent, false))
         }
 
+        @SuppressLint("ClickableViewAccessibility")
         override fun onBindViewHolder(holder: RecyclerView.ViewHolder, position: Int) {
             if (holder is TokenViewHolder) {
-                TokenLogoUtil.setLogo(tokenList[position], mActivity, holder.tokenImage)
-                holder.tokenName.text = tokenList[position].name
-                holder.tokenSymbol.text = tokenList[position].symbol
-                if (!TextUtils.isEmpty(tokenList[position].contractAddress)) {
-                    holder.tokenContractAddress.text = tokenList[position].contractAddress
+                TokenLogoUtil.setLogo(mTokenList[position], mActivity, holder.mIvTokenImage)
+                holder.mTvTokenName.text = mTokenList[position].name
+                holder.mTvTokenSymbol.text = mTokenList[position].symbol
+                if (!TextUtils.isEmpty(mTokenList[position].contractAddress)) {
+                    holder.mTvTokenContractAddress.text = mTokenList[position].contractAddress
                 } else {
-                    holder.tokenContractAddress.visibility = View.GONE
+                    holder.mTvTokenContractAddress.visibility = View.GONE
                 }
-                holder.tokenSelectSwitch.setOnCheckedChangeListener(null)
-                holder.tokenSelectSwitch.isChecked = tokenList[position].selected
+                holder.mSwtTokenSelect.setOnCheckedChangeListener(null)
+                holder.mSwtTokenSelect.isChecked = mTokenList[position].selected
 
-                holder.tokenSelectSwitch.setOnCheckedChangeListener { _, isChecked ->
-                    tokenList[position].selected = isChecked
-                    if (isChecked) {
-                        DBWalletUtil.updateTokenToCurrentWallet(mActivity, tokenList[position])
-                    } else {
-                        DBWalletUtil.updateTokenToCurrentWallet(mActivity, tokenList[position])
+                holder.mSwtTokenSelect.setOnCheckedChangeListener { _, isChecked ->
+                    mTokenList[position].selected = isChecked
+                }
+                holder.mRlRoot.setOnTouchListener { _, event ->
+                    if (event.action == MotionEvent.ACTION_DOWN) {
+                        if (!mTokenStatus) {
+                            mItemTouchHelper.startDrag(holder)
+                        }
                     }
+                    false
+                }
+                if (mTokenStatus) {
+                    holder.mSwtTokenSelect.visibility = View.VISIBLE
+                    holder.mIvTokenDrag.visibility = View.GONE
+                } else {
+                    holder.mSwtTokenSelect.visibility = View.GONE
+                    holder.mIvTokenDrag.visibility = View.VISIBLE
                 }
                 holder.itemView.tag = position
             }
         }
 
         override fun getItemCount(): Int {
-            return tokenList.size
+            return mTokenList.size
+        }
+
+        fun exchange(position1: Int, position2: Int) {
+            var item1 = mTokenList[position1]
+            var item2 = mTokenList[position2]
+            mTokenList.removeAt(position1)
+            mTokenList.add(position1, item2)
+            mTokenList.removeAt(position2)
+            mTokenList.add(position2, item1)
+            notifyItemMoved(position1, position2)
         }
 
         internal inner class TokenViewHolder(view: View) : RecyclerView.ViewHolder(view) {
-            var tokenImage: ImageView = view.findViewById(R.id.token_image)
-            var tokenName: TextView = view.findViewById(R.id.token_name_text)
-            var tokenSymbol: TextView = view.findViewById(R.id.token_symbol_text)
-            var tokenContractAddress: TextView = view.findViewById(R.id.token_contract_address)
-            var tokenSelectSwitch: Switch = view.findViewById(R.id.switch_token_select)
+            var mIvTokenImage: ImageView = view.findViewById(R.id.token_image)
+            var mTvTokenName: TextView = view.findViewById(R.id.token_name_text)
+            var mTvTokenSymbol: TextView = view.findViewById(R.id.token_symbol_text)
+            var mTvTokenContractAddress: TextView = view.findViewById(R.id.token_contract_address)
+            var mSwtTokenSelect: Switch = view.findViewById(R.id.switch_token_select)
+            var mIvTokenDrag: ImageView = view.findViewById(R.id.iv_drag)
+            var mRlRoot: RelativeLayout = view.findViewById(R.id.rl_root)
         }
     }
 
-    override fun onKeyDown(keyCode: Int, event: KeyEvent): Boolean {
-        if (keyCode == KeyEvent.KEYCODE_BACK && event.action == KeyEvent.ACTION_DOWN) {
-            EventBus.getDefault().post(AddTokenRefreshEvent())
-            finish()
+    override fun finish() {
+        var walletItem = DBWalletUtil.getCurrentWallet(mActivity)
+        walletItem.tokenItems = mTokenList
+        DBWalletUtil.saveWallet(mActivity, walletItem)
+        EventBus.getDefault().post(AddTokenRefreshEvent())
+        super.finish()
+    }
+
+    inner class SimpleItemTouchCallback : ItemTouchHelper.Callback() {
+        override fun getMovementFlags(recyclerView: RecyclerView?, viewHolder: RecyclerView.ViewHolder?): Int {
+            val dragFlag = ItemTouchHelper.LEFT or ItemTouchHelper.RIGHT or ItemTouchHelper.UP or ItemTouchHelper.DOWN
+            val swipeFlag = 0
+            return ItemTouchHelper.Callback.makeMovementFlags(dragFlag, swipeFlag)
+        }
+
+        override fun onMove(recyclerView: RecyclerView?, viewHolder: RecyclerView.ViewHolder?, target: RecyclerView.ViewHolder?): Boolean {
+            mAdapter.exchange(viewHolder!!.adapterPosition, target!!.adapterPosition)
             return true
         }
-        return super.onKeyDown(keyCode, event)
+
+        override fun onSwiped(viewHolder: RecyclerView.ViewHolder?, direction: Int) {
+        }
+
+        override fun isLongPressDragEnabled(): Boolean {
+            return !mTokenStatus
+        }
+
     }
 
 }
